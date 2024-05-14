3c310627ad4a270333914829646382e1746e9bbc
chuyen <dautubenvungctv@gmail.com>
2024-05-14 16:56:02 +0700

    third commit

--- src/component/Layout/Carousel/Carousel.tsx
+++ src/component/Layout/Carousel/Carousel.tsx
@@ -42,99 +42,39 @@ export const CarouselHome = () => {
   return (
     <StyledCarousel>
       {" "}
-      <Carousel
-        additionalTransfrom={0}
-        arrows
-        autoPlay
-        autoPlaySpeed={5000}
-        centerMode={false}
-        className=""
-        containerClass="container-with-dots"
-        dotListClass=""
-        draggable
-        focusOnSelect={false}
-        infinite={false}
-        itemClass=""
-        keyBoardControl
-        minimumTouchDrag={80}
-        pauseOnHover
-        renderArrowsWhenDisabled={false}
-        renderButtonGroupOutside={false}
-        renderDotsOutside={false}
-        responsive={{
-          superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 3000, min: 1200 },
-            items: 3,
-          },
-          desktop: {
-            breakpoint: {
-              max: 1200,
-              min: 1024,
-            },
-            items: 3,
-            partialVisibilityGutter: 40,
-          },
-          tablet: {
-            breakpoint: {
-              max: 1024,
-              min: 586,
-            },
-            items: 3,
-            partialVisibilityGutter: 30,
-          },
-          mobile: {
-            breakpoint: {
-              max: 586,
-              min: 0,
-            },
-            items: 2,
-            partialVisibilityGutter: 30,
-          },
-        }}
-        rewind
-        rewindWithAnimation={false}
-        rtl={false}
-        shouldResetAutoplay
-        showDots={false}
-        sliderClass=""
-        slidesToSlide={2}
-        swipeable
-      >
-        {listCourse.map((item: any, index: any) => (
-          <div>
-            <div
-              style={{ cursor: "pointer" }}
-              onClick={() => handleDelete(item.course_id)}
-            >
-              <IoIosCloseCircleOutline />
-            </div>
-
-            <Link
-              to={`/product-detail/${item?.course_id}`}
-              className="item-caroulsel"
-            >
-              <div className="title">
-                <img
-                  style={{
-                    width: "100%",
-                    height: "100px",
-                    borderRadius: "3px",
-                  }}
-                  src={item?.image}
-                  alt=""
-                />
-              </div>
-              <div className="body">
-                <div className="body-first">
-                  <div className="item-first">{item?.title}</div>
-                  <div className="price">{item?.price}₫</div>
-                </div>
-              </div>
-            </Link>
+      {listCourse.map((item: any, index: any) => (
+        <div>
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={() => handleDelete(item.course_id)}
+          >
+            <IoIosCloseCircleOutline />
           </div>
-        ))}
-      </Carousel>
+
+          <Link
+            to={`/product-detail/${item?.course_id}`}
+            className="item-caroulsel"
+          >
+            <div className="title">
+              <img
+                style={{
+                  width: "100%",
+                  height: "100px",
+                  borderRadius: "3px",
+                }}
+                src={item?.image}
+                alt=""
+              />
+            </div>
+            <div className="body">
+              <div className="body-first">
+                <div className="item-first">{item?.title}</div>
+                <div className="price">{item?.price}₫</div>
+              </div>
+            </div>
+          </Link>
+        </div>
+      ))}
       <button onClick={handleAdd}>Thêm sản phẩm</button>
     </StyledCarousel>
   );
