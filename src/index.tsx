cdaa93e1bbe48f1a5f9596e5c4c81fca7fe18e5c
chuyen <dautubenvungctv@gmail.com>
2024-07-18 10:35:17 +0700

    zalo

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+declare global {
+  interface Window {
+    zaloJSV2: any;
+  }
+}
+window.zaloJSV2 = {
+  zalo_h5_event_handler: function () {},
+};
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
